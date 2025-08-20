668ec686125345750811a299100304fedcbdf76d
MNS <sanjayajnas447@gmail.com>
2025-08-21 00:55:13 +0530

    Update miniStoreMonitor.c

--- miniStoreMonitor.c
+++ miniStoreMonitor.c
@@ -1,41 +1,53 @@
 #include<stdio.h>
 #include<string.h>
 
+/*initialising store - Multi dimensional char array to sotre upto 100 item names.
+    1D integer array quantity to keep track of the quantity of the product.
+    cur denotes the end of the array and if cur==-1 means there's no element in the store array at that moment.
+*/
 char store[100][25];
 int cur=-1,quantity[100];
+
+//function to search for the required item
 int search(char sitem[25]){
-    if(cur==-1){
+    if(cur==-1){     //store array is empty
         return -1;
     }
     for(int i=0;i<=cur;i++){
         if(strcmp(sitem,store[i])==0){
-            return i;
+            return i;             //returning index of the item at the store array 
         }
     }
-    return -1;
+    return -1;    // item not found
 }
+
+//function to add item in store array
 void additem(){
     if(cur==99){
-        printf("\nStore full, delete any item first");
+        printf("\nStore full, delete any item first");     //store array full
         return;
     }
-    printf("\nEnter the product name: ");
+
+    //adding the item directly into the store array and updating its corresponding quantity
+    printf("\nEnter the product name: ");     
     scanf("%s",&store[++cur]);
     printf("\nEnter the product quantity: ");
     scanf("%d",&quantity[cur]);
     printf("\nItem Added Succesfully");
 }
+
+//function to delete an item
 void deleteitem(char ditem[25]){
-    if(cur==-1){
+    if(cur==-1){          // no item to delete
         printf("\nNo item in store");
         return;
     }
-    int idx=search(ditem);
+    int idx=search(ditem);    // we use our search fucntion to help us know whether the item exists or not, also if it exists it gives us the position of the item.
     if(idx==-1){
         printf("\nNo such item exist in the store");
         return;
     }
-    for(int i=idx;i<cur-1;i++){
+    for(int i=idx;i<cur-1;i++){            //left shift every item by 1 position to remove the item from the store array
         strcpy(store[i],store[i+1]);
         quantity[i]=quantity[i+1];
     }
@@ -45,6 +57,8 @@ void deleteitem(char ditem[25]){
 void main(){
     int idx,choice;
     char sitem[25],ditem[25];
+    
+    //Menu
     printf("Options Available:-\n1.Add Item\n2.Search Item\n3.Delete Item\n4.Exit");
     while(1){
         printf("\nEnter your choice: ");
